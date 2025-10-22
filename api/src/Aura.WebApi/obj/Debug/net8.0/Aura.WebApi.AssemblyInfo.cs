704503725181e498bf90122db6fc34de90c41ec5
mimunozc <mimunozc@duocuc.cl>
2025-10-21 23:55:12 -0300

    Modificaciones y arranque local (IA + API + Front)

--- api/src/Aura.WebApi/obj/Debug/net8.0/Aura.WebApi.AssemblyInfo.cs
+++ api/src/Aura.WebApi/obj/Debug/net8.0/Aura.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aura.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d91922a92639f0923b3177841a1b9a1348330db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502a3ef4affc43518870a6430ebd5bfd0359463d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aura.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aura.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
